977ea8ca8289b26b81e17895ce23360f58443ae7
Bhaa Shakur <bhaa@vastdata.com>
2021-02-17 21:36:40 +0200

    Add 'ignore_results'. arrange code

--- examples.py
+++ examples.py
@@ -1,15 +1,17 @@
 import random
-import sys
 import time
+import unittest
 
 from multiprocess_pipeline import MultiProcessPipeline, logger
+
+randomization = False
 
 
 def start(item):
     if random.choice(range(10)) in (1, 2):
         time.sleep(0.3)
     logger.info(f"start handling {item}")
-    return item, random.choice(range(10))
+    return item, random.choice(range(10)) if randomization else item
 
 
 def process(data):
@@ -17,6 +19,8 @@ def process(data):
     if random.choice(range(10)) in (1, 2, 3, 4):
         time.sleep(0.1)
     logger.info(f"processing {item} with value {value}")
+    if randomization:
+        return (item,) * 3
     return item, random.choice(range(50, 200)), random.choice([True, False])
 
 
@@ -25,25 +29,35 @@ def report(data):
         time.sleep(0.4)
     item, result, info = data
     logger.info(f"Reporting {item}: result: {result}. additional info={info}")
-    return random.choice(range(500, 1000))
+    return random.choice(range(500, 1000)) if randomization else item
 
 
-def ex1():
-    num, items = 10, list(range(1, 50 + 1))
-    MultiProcessPipeline([(start, num), (process, num), (report, num)], collection=items)()
+class PipelineTests(unittest.TestCase):
 
+    def test_sanity(self):
+        items = list(range(1, 3))
+        multi = MultiProcessPipeline([(start, 1), (process, 1), (report, 1)], collection=items)
+        multi.start()
+        multi.join(ignore_results=True)
 
-def ex2():
-    items = list(range(1, 1000 + 1))
-    multi = MultiProcessPipeline([(start, 3), (process, 3), (report, 3)], collection=items)
-    multi.start()
-    multi.join(ignore_results=True)
+    def _test_long(self):
+        items = list(range(1, 500 + 1))
+        multi = MultiProcessPipeline([(start, 10), (process, 10), (report, 10)], collection=items)
+        multi.start()
+        multi.join(ignore_results=True)
 
+    def test_sorted(self):
+        global randomization
+        orig_value = randomization
+        try:
+            randomization = False
+            items = list(range(1, 100 + 1))
+            multi = MultiProcessPipeline([(start, 3), (process, 4), (report, 2)], collection=items)
+            multi.start()
+            results = multi.join()
 
-def main():
-    # ex1()
-    ex2()
-
-
-if __name__ == '__main__':
-    sys.exit(main())
+            results = list(results)
+            logger.info(results)
+            assert results == items
+        finally:
+            randomization = orig_value
---
+++ multiprocess_pipeline/__init__.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+import itertools
+import logging
+import multiprocessing
+from multiprocessing.queues import JoinableQueue
+
+__all__ = ['MultiProcessPipeline', 'logger']
+
+
+def init_logger():
+    handler = logging.StreamHandler()
+    log_format = u'%(asctime)s [%(levelname)-1s %(process)d %(threadName)s]  %(message)s'
+    handler.setFormatter(logging.Formatter(log_format))
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+
+logger = logging.getLogger("MultiProcessPipeline")
+init_logger()
+
+
+class MultiProcessPipeline:
+
+    def __init__(self, funcs, collection, default_process_num=1):
+        self.collection = collection
+        # communication queues
+        self.queues = [JoinableQueue(maxsize=-1, ctx=multiprocessing.get_context()) for _ in range(len(funcs) + 1)]
+
+        first_func, start_size, current_size = None, 0, 0
+
+        self.processes = []
+        for idx, data in enumerate(funcs):
+            current_func, current_size = self.get_current_info(data, default_process_num)
+            next_func, next_size = self.get_next_info(funcs, idx, default_process_num)
+
+            assert callable(current_func), f"Function '{current_func}' is not a callable"
+
+            readq, writeq = self.queues[idx], self.queues[idx + 1]
+            barrier = multiprocessing.Barrier(current_size)
+            start_size = start_size or max(1, current_size)
+            first_func = first_func or current_func
+
+            self.processes.append([MultiProcessPipeline.Consumer(readq, writeq, barrier, num_stops, next_func)
+                                   for i, num_stops in enumerate(self.get_num_stops(current_size, next_size))])
+
+        self.end_size = current_size
+        self.start_size = start_size
+        self.start_func = first_func
+
+    @staticmethod
+    def get_current_info(data, default_process_num):
+        if isinstance(data, tuple):
+            current_func, current_size = data
+        else:
+            current_func, current_size = data, default_process_num
+        return current_func, current_size
+
+    @staticmethod
+    def get_next_info(funcs, idx, default_process_num):
+        try:
+            next_func, next_size = funcs[idx + 1]
+        except TypeError:
+            next_func, next_size = (funcs[idx + 1], default_process_num)
+        except IndexError:
+            next_func, next_size = None, None
+        return next_func, next_size
+
+    def start(self):
+        for p in itertools.chain.from_iterable(self.processes):
+            p.start()
+        start_queue = self.queues[0]
+        for i, item in enumerate(self.collection):
+            start_queue.put(MultiProcessPipeline.Task(self.start_func, item, index=i))
+        for _ in range(self.start_size):
+            start_queue.put(None)
+
+    def join(self, ignore_results=False):
+        # Skip joining the last queue - no one is taking from it
+        for q in self.queues[:1]:
+            q.join()
+
+        last_q = self.queues[-1]
+        stops, results = 0, []
+        while stops < self.end_size:
+            result_task = last_q.get()
+            if result_task is None:
+                stops += 1
+            elif not ignore_results:
+                results.append(result_task)
+        return (t.result for t in sorted(results, key=lambda t: t.index))
+
+    @staticmethod
+    def get_num_stops(x, y):
+        integer, reminder = (1, 0) if y is None else (int(y / x), y % x)
+        for i in range(x):
+            yield integer + (0 if i else reminder)
+        raise StopIteration()
+
+    class Task:
+        def __init__(self, func, arg, index):
+            self.func = func
+            self.arg = arg
+            self.result = arg
+            self.index = index
+
+        def __call__(self):
+            if self.func:
+                self.result = self.func(self.arg)
+            return self
+
+        def __str__(self):
+            return f'[{self.index}] {self.result}'
+
+    class Consumer(multiprocessing.Process):
+
+        def __init__(self, readq, writeq, barrier, num_pills, next_func):
+            super().__init__()
+            self.readq = readq
+            self.writeq = writeq
+            self.num_pills = num_pills
+            self.barrier = barrier
+            self.next_func = next_func
+
+        def run(self):
+            # proc_name = self.name
+            # Poison pill means shutdown
+            for next_task in iter(self.readq.get, None):
+                task = next_task()
+                self.readq.task_done()
+                self.writeq.put(MultiProcessPipeline.Task(self.next_func, task.result, task.index))
+
+            self.readq.task_done()
+            self.barrier.wait()
+            for _ in range(self.num_pills):
+                self.writeq.put(None)
